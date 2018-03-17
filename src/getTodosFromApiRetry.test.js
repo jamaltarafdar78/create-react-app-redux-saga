2994e1ebda722d2ce494d48665528600b6bd254c
Jamal H. Tarafdar <jamal.tarafdar@gmail.com>
2018-03-17 20:57:00 +0000

    feature/add retry if service fails to provide todos on first attempt

---
+++ src/getTodosFromApiRetry.test.js
@@ -0,0 +1,32 @@
+import { expectSaga } from 'redux-saga-test-plan';
+import { throwError } from 'redux-saga-test-plan/providers';
+import * as Services from './services/todo-services';
+import { getTodosWithRetry, getTodosFromApi, retryGetTodosAfter5Secs} from './saga';
+import { delay } from 'redux-saga';
+import {call} from 'redux-saga/effects';
+
+test('getTodosWithRetry retries to get todos after 5 secs if error from service', async () => {
+    const error = new Error("getTodos failed!");
+
+    return expectSaga( getTodosWithRetry )
+        .provide([
+            [   call(getTodosFromApi), 
+                throwError(error)
+            ]
+        ])
+        .call(retryGetTodosAfter5Secs)
+        .silentRun();
+});
+
+test('getTodosWithRetry does NOT retry to get todos if success from service', async () => {
+    const todos = ["todos1", "todos2"];
+
+    return expectSaga( getTodosWithRetry )
+        .provide([
+            [   call(getTodosFromApi), 
+                todos
+            ]
+        ])
+        .not.call(retryGetTodosAfter5Secs)
+        .silentRun();
+});
--- src/saga.js
+++ src/saga.js
@@ -1,11 +1,13 @@
 import {
-    all, 
-    put, 
+    all,
     call,
+    put, 
     takeEvery,
     throttle
 } from 'redux-saga/effects';
-import {getTodos} from './services/todo-services';
+
+import { delay } from 'redux-saga';
+import { getTodos } from './services/todo-services';
 import { loadTodos, updateCurrent } from './actions/sync-actions';
 import * as AsyncActions from './actions/async-actions';
 
@@ -15,6 +17,24 @@ export function* getTodosFromApi() {
     console.log(`recieved ${JSON.stringify(todos)}`);
 
     return yield put(loadTodos(todos));
+}
+
+export function* retryGetTodosAfter5Secs(){
+    yield call(delay, 5000);
+
+    console.log('retrying to get Todos!');
+    yield call(getTodosFromApi);
+}
+
+export function* getTodosWithRetry(){
+    try {
+        console.log('trying to get Todos!');
+        yield call(getTodosFromApi);
+    }
+    catch(e) {
+        console.log('failed to get Todos, retrying in 5secs!');
+        yield call(retryGetTodosAfter5Secs);
+    }
 }
 
 function* updateCurrentReducer({payload}){
@@ -35,7 +55,7 @@ function* watchGetTodosFromService() {
     console.log('watching for GET_TODOS_FROM_SERVICE')
     yield takeEvery(
         AsyncActions.GET_TODOS_FROM_SERVICE, 
-        getTodosFromApi
+        getTodosWithRetry
     );
 }
 
