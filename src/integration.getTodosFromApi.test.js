72c34a93a41a38fa1b471b843c9e10de76feabf1
Jamal H. Tarafdar <jamal.tarafdar@gmail.com>
2018-03-17 18:20:40 +0000

    test/ASYNC middleware for successful getTodosFromApi ultimately updates state correctly

---
+++ src/integration.getTodosFromApi.test.js
@@ -0,0 +1,36 @@
+import sinon from 'sinon';
+import * as Services from './services/todo-services';
+import { runSaga } from 'redux-saga';
+import {getTodosFromApi} from './saga';
+import {LOAD_TODOS} from './actions/sync-actions';
+import {createStore, applyMiddleware} from 'redux';
+import reducer from './reducers/todo';
+import createSagaMiddleware from 'redux-saga';
+
+test('test SUCCESSFUL service call to getTodosFromApi ULTIMATELY causes successful update to state', async () => {
+    const todos = ['todo1', 'todo2'];
+
+    const sagaMiddleware = createSagaMiddleware();
+
+    const store = createStore(
+        reducer,
+        applyMiddleware(sagaMiddleware)
+    );
+    
+    sinon.stub(Services, 'getTodos').callsFake(() => todos);
+
+    const saga = await runSaga(
+        {
+            dispatch: action  => store.dispatch(action),
+            getState: () => store.getState()
+        },
+        getTodosFromApi
+    );
+
+    expect(store.getState()).toEqual(
+        {
+            todos,
+            currentTodo: ''
+        }
+    )
+})
